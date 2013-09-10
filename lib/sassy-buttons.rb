aa9d28bcd64564499f2b8400c1ab926806fdfd88
Jared Hardy <jared@jaredhardy.com>
2013-09-10 11:03:35 -0700

    remove files

--- lib/sassy-buttons.rb
+++
@@ -1,4 +0,0 @@
-require 'compass'
-Compass::Frameworks.register('sassy-buttons',
-                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'stylesheets'),
-                             :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
